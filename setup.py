1ddd67f731a0b4c304cf4ee4f386d29778a0b9b0
Jose GomezDans <jgomezdans@gmail.com>
2012-04-25 18:00:44 +0100

    Installation stuff

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='SPSA',
+      version='1.0',
+      description='Simultaneous Perturbation Stochastic Approximation optimisation',
+      author='Jose Gomez-Dans',
+      author_email='j.gomez-dans@ucl.ac.uk',
+      url='http://github.com/jgomezdans/spsa',
+      packages=['spsa'],
+     )
---
+++ spsa/__init__.py
@@ -0,0 +1,1 @@
+from SPSA import SimpleSPSA 
